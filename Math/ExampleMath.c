c48ba85dc30c43d3dbee269807d80f9765c7b7af
Jack Frank <jf679@cornell.edu>
2024-04-11 23:39:26 -0400

    to c

---
+++ Math/ExampleMath.c
@@ -0,0 +1,14 @@
+#include "ExampleMath.h"
+
+double root(double x)
+{
+    return sqrt(x);
+}
+double square(double x)
+{
+    return x * x;
+}
+double sin(double x)
+{
+    return sin(x);
+}
--- Math/ExampleMath.h
+++ Math/ExampleMath.h
@@ -1,7 +1,5 @@
 
-namespace examplemath
-{
-    double root(double x);
-    double square(double x);
-    double sin(double x);
-}
+
+double root(double x);
+double square(double x);
+double sin(double x);
